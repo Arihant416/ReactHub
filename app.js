746035cc1fe905a23878c93a68f822cbead13041
Arihant416 <arihantjain416@gmail.com>
2020-06-10 10:51:29 +0530

    Mongoose connection established

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express'),
    app = express(),
-   PORT = 2500;
+   PORT = 2500,
+   mongoose = require('mongoose'),
+   { mongoURL } = require('./config/key');
 
+require('./models/userModel');
 
-const checkMiddleWare = () => {
-   console.log('Checking middleware');
-
-}
-app.use(checkMiddleWare)
+mongoose.connect(mongoURL,
+   { useNewUrlParser: true, useUnifiedTopology: true }
+)
+mongoose.connection.on('connected', () => {
+   console.log('Atlas connected');
+})
+mongoose.connection.on('error', () => {
+   console.log('Failed to connect Atlas')
+})
 app.get('/', (req, res) => {
    res.send('Hello user');
 })
-
-
 app.listen(PORT, () => {
    console.log(`Listening on port ${PORT}`);
 })
