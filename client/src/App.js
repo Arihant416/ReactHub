45d1922208218d17a55e7728fac7cb8de32319ff
Arihant416 <arihantjain416@gmail.com>
2020-06-12 13:32:56 +0530

    Login Page Set Up Complete Token Stored

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './myComponents/Header';
 import Landing from './myComponents/Landing';
+import Login from './myComponents/Login';
 import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <Route exact path='/' component={Landing} />
+      <Route path='/login' component={Login} />
     </BrowserRouter>
   );
 }
